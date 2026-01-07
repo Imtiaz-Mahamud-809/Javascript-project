6e800ddedec6fe799ddd5654e0d098115ecf3a5f
Imtiaz Mahamud <2253091187@uttara.ac.bd>
2026-01-08 00:14:15 +0600

    console
    added

---
+++ js/script.js
@@ -0,0 +1,184 @@
+alert("Thank You For Visiting");
+
+console.log("I Am Totally Connected");
+
+var Random = "anyone";
+var People = true;
+var age = 30;
+console.log(Random);
+console.log(People);
+console.log(age);
+
+{
+  var solar_system = "sun";
+}
+console.log(solar_system);
+
+{
+  var weather = "winter";
+  console.log(weather);
+}
+
+let x = 15;
+x = 30;
+console.log(x);
+
+{
+  let x = 11;
+  let y = 12;
+  console.log(x);
+  console.log(y);
+  console.log(x + y);
+}
+
+{
+  let x = 10;
+  let y = 450;
+  let z = 20;
+  let w = 80;
+  console.log(x + y);
+  console.log(x - y);
+  console.log(w / z);
+  console.log(w * z);
+}
+
+{
+  let variable = "scope";
+  console.log("Let ", variable);
+}
+{
+  let variable = "scope ";
+  console.log("Let" + " " + variable + "using variable");
+}
+
+{
+  let variable = "scope";
+  console.log(typeof variable);
+}
+{
+  let variable = 2507;
+  console.log(typeof variable);
+}
+{
+  let variable = false;
+  console.log(typeof variable);
+}
+
+const pi = 3.141617;
+console.log(pi);
+
+{
+  let multiplication_table = 16;
+  let number = "multiplication table";
+  console.log(number);
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "1" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 1
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "2" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 2
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "3" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 3
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "4" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 4
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "5" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 5
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "6" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 6
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "7" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 7
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "8" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 8
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "9" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 9
+  );
+  console.log(
+    multiplication_table +
+      " " +
+      "x" +
+      " " +
+      "10" +
+      " " +
+      "=" +
+      " " +
+      multiplication_table * 10
+  );
+}
